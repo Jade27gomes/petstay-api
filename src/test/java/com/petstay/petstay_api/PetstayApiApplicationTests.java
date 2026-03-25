104129055c5925b10a54ea97a99094ac16395679
Jade27gomes <js27gomes@gmail.com>
2026-03-25 11:24:54 -0300

    spring initializar structure

---
+++ src/test/java/com/petstay/petstay_api/PetstayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petstay.petstay_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
